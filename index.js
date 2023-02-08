2f0fc0b90a53e59282f4f4df0199af5a3eb1868f
nileshghavate <nsghavate@gmail.com>
2023-02-08 10:39:50 +0530

    SQL_Query_Issue1_resolved

--- index.js
+++ index.js
@@ -215,34 +215,38 @@ app.post("/import-excel", uploadFile.single("students"), (req, res) => {
 
 
 //date and time
-function getDateTime() {
-  var now = new Date();
-  var year = now.getFullYear();
-  var month = now.getMonth() + 1;
-  var day = now.getDate();
-  var hour = now.getHours();
-  var minute = now.getMinutes();
-  var second = now.getSeconds();
-  if (month.toString().length == 1) {
-    month = "0" + month;
-  }
-  if (day.toString().length == 1) {
-    day = "0" + day;
-  }
-  if (hour.toString().length == 1) {
-    hour = "0" + hour;
-  }
-  if (minute.toString().length == 1) {
-    minute = "0" + minute;
-  }
-  if (second.toString().length == 1) {
-    second = "0" + second;
-  }
-  var dateTime =
-    day + "/" + month + "/" + year + " " + hour + ":" + minute + ":" + second;
-  return dateTime;
-}
-console.log("date:", getDateTime());
+// function getDateTime() {
+//   var now = new Date();
+//   var year = now.getFullYear();
+//   var month = now.getMonth() + 1;
+//   var day = now.getDate();
+//   var hour = now.getHours();
+//   var minute = now.getMinutes();
+//   var second = now.getSeconds();
+//   if (month.toString().length == 1) {
+//     month = "0" + month;
+//   }
+//   if (day.toString().length == 1) {
+//     day = "0" + day;
+//   }
+//   if (hour.toString().length == 1) {
+//     hour = "0" + hour;
+//   }
+//   if (minute.toString().length == 1) {
+//     minute = "0" + minute;
+//   }
+//   if (second.toString().length == 1) {
+//     second = "0" + second;
+//   }
+//   var dateTime =
+//     day + "/" + month + "/" + year + " " + hour + ":" + minute + ":" + second;
+//   return dateTime;
+// }
+
+var now = new Date().toLocaleString('en-US', {
+  timeZone: 'Asia/Calcutta'
+});
+console.log("date:", now);
 
 console.log("dirname:",__dirname);
 //creating server
