89d8692cbeeb15f864ebc56e30fa3ea6ad87ec71
tushar4303 <padhytushar@protonmail.com>
2023-02-28 23:39:20 +0530

    updated app api route + token gets updated in db

--- controllers/appControllers/authAppUser.js
+++ controllers/appControllers/authAppUser.js
@@ -14,20 +14,21 @@ module.exports = {
 
     // Check if username and password are provided
     if (!usermobno || !password) {
-      // console.log("idhar");
+      console.log("idhar");
       return res.status(400).json({ data: null, success: "false", message: 'Username and password are required.' });
     }
 
 
-    const qry = `SELECT * FROM cluedin.user_details where user_mobno ="${usermobno}";`;
+    const qry = `SELECT * FROM cluedin.user_details WHERE user_mobno = "${usermobno}"`;
 
     pool.query(qry, (err, result) => {
       console.log(qry);
-      // console.log(result);
-      if (err) res.send(err);
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ data: null, success: false, message: 'Internal server error.' });
+      }
       if (result.length === 0) {
-        // console.log("idhar 2");
-        return res.status(401).json({ data: null, success: "false", msg: 'Invalid username or password.' });
+        return res.status(401).json({ data: null, success: false, message: "The user doesn't exist. Please Signup to continue" });
       }
 
       // Check if the password is correct
@@ -35,12 +36,23 @@ module.exports = {
       const user = result[0];
       console.log(user);
       if (user.user_pwd !== password) {
-        // console.log("idhar 3");
         return res.status(401).json({ data: null, success: "false", msg: 'Invalid username or password.' });
       }
-      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: user.user_mobno }, process.env.JWT_SECRET);
+
+      // Store the token in the server's database for single user login
+      const updateTokenQuery = `UPDATE cluedin.user_details SET user_token = "${token}" WHERE user_mobno = "${user.user_mobno}";`;
+      pool.query(updateTokenQuery, (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({ data: null, success: false, message: 'Internal server error.' });
+        }
+        console.log(`Token has been stored in the database for user ${usermobno}`);
+      });
+
       return res.status(200).json({ data: { token }, success: "true", msg: 'User has been authenticated successfully.' });
 
     });
   },
+
 };
