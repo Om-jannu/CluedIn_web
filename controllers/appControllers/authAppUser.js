4b436152c9ee54bf0b5ec50d56ea795f6bcd0c5d
jasmit21 <jasmitrathod21@gmail.com>
2023-02-28 19:32:02 +0000

    auth api modified

--- controllers/appControllers/authAppUser.js
+++ controllers/appControllers/authAppUser.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json);
 const jwt = require('jsonwebtoken');
+const { use } = require("../importExcelController");
 
 module.exports = {
   post: (req, res) => {
@@ -19,8 +20,8 @@ module.exports = {
     }
 
 
-    const qry = `SELECT * FROM cluedin.user_details WHERE user_mobno = "${usermobno}"`;
-
+    const qry = `SELECT t1.* , t2.branch_name FROM cluedin.user_details t1 , branch t2 WHERE user_mobno = "${usermobno}" and t1.user_department=t2.branch_id`;
+    // const qry1 = `SELECT t1.user_fname,t1.user_lname,t1.user_mobno,t1.user_pwd,t1.user_email,t2.branch_name , t1.user_profilePic from user_details t1 , branch t2 where t1.user_mobno="${mobno}" and t1.user_department=t2.branch_id;`
     pool.query(qry, (err, result) => {
       console.log(qry);
       if (err) {
@@ -49,8 +50,28 @@ module.exports = {
         }
         console.log(`Token has been stored in the database for user ${usermobno}`);
       });
-
-      return res.status(200).json({ data: { token }, success: "true", msg: 'User has been authenticated successfully.' });
+      //local variables to store profile data 
+      let user_fname = user.user_fname;
+      let user_lname = user.user_lname;
+      let user_mobno = user.user_mobno;
+      let user_email = user.user_email;
+      let user_branch = user.branch_name;
+      let user_profilePic = user.user_profilePic;
+      // console.log(user_email);
+      return res.status(200).json(
+        { data: 
+          { 
+            user_fname,
+            user_lname,
+            user_mobno,
+            user_email,
+            user_branch,
+            user_profilePic,
+            token
+           }, 
+           success: "true",
+            msg: 'User has been authenticated successfully.' 
+        });
 
     });
   },
